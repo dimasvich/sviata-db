64d0da47ac02ecfb9125f30f4bff021aa0813d72
dimasvich <37069199+dimasvich@users.noreply.github.com>
2025-11-06 22:56:25 +0200

    Add files via upload

--- client/src/pages/add-info/index.tsx
+++ client/src/pages/add-info/index.tsx
@@ -147,7 +147,7 @@ export default function AddInfo() {
   };
   useEffect(() => {
     if (sviato.mainImage) {
-      setMainImageUrl(`${baseUrl}/uploads/${id}/${sviato.mainImage}`);
+      setMainImageUrl(`${baseUrl}/uploads/${id}/main/${sviato.mainImage}`);
     }
   }, [sviato]);
   useEffect(() => {
