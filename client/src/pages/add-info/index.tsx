9d99f7acb2003dad88e94367711f3b591411e9a9
dimasvich <37069199+dimasvich@users.noreply.github.com>
2025-11-06 11:13:52 +0200

    Add files via upload

--- client/src/pages/add-info/index.tsx
+++ client/src/pages/add-info/index.tsx
@@ -24,8 +24,9 @@ import 'dayjs/locale/uk';
 import localeData from 'dayjs/plugin/localeData';
 import Head from 'next/head';
 import { useRouter, useSearchParams } from 'next/navigation';
-import { useEffect, useState } from 'react';
+import { useEffect, useMemo, useState } from 'react';
 
+dayjs.locale('uk');
 dayjs.extend(localeData);
 
 export default function AddInfo() {
@@ -393,6 +394,31 @@ export default function AddInfo() {
     }
   }, [dayOfWeek, month, week]);
 
+  const weekdays = [
+    'Sunday',
+    'Monday',
+    'Tuesday',
+    'Wednesday',
+    'Thursday',
+    'Friday',
+    'Saturday',
+  ];
+
+  const months = [
+    'January',
+    'February',
+    'March',
+    'April',
+    'May',
+    'June',
+    'July',
+    'August',
+    'September',
+    'October',
+    'November',
+    'December',
+  ];
+
   return (
     <>
       <Head>
@@ -423,7 +449,7 @@ export default function AddInfo() {
                       value={dayOfWeek}
                       onChange={setDayOfWeek}
                       label="День тижня"
-                      options={dayjs.weekdays()}
+                      options={weekdays}
                       error=""
                     />
                     <Select
@@ -439,7 +465,7 @@ export default function AddInfo() {
                       value={month}
                       onChange={setMonth}
                       label="Місяць"
-                      options={dayjs.months()}
+                      options={months}
                       error=""
                     />
                   </div>
